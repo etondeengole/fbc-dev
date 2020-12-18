91ea320e230b5fee2bc6cce767cbd4120dc78972
Etonde Ngole <etonde.ngole@gmail.com>
2020-12-18 06:00:17 +0200

    Added email component

--- FaithBibleInternationalChurch/src/app/app.module.ts
+++ FaithBibleInternationalChurch/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { FormsModule } from '@angular/forms';
     EventsCalendarComponent,
     CountdownTimerComponent,
     SiteFooterComponent,
-    ContactFormComponent
+    ContactFormComponent    
   ],
   imports: [
     BrowserModule,
