56f5607b27d73211a2878cc88e018370679dc844
Etonde Ngole <etonde.ngole@gmail.com>
2021-04-05 09:14:31 +0200

    assignable from component

--- FaithBibleInternationalChurch/src/app/live-stream/live-stream.component.ts
+++ FaithBibleInternationalChurch/src/app/live-stream/live-stream.component.ts
@@ -1,6 +1,7 @@
 
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { EmbedVideoService } from 'ngx-embed-video';
 
 @Component({
@@ -9,16 +10,17 @@ import { EmbedVideoService } from 'ngx-embed-video';
   styleUrls: ['./live-stream.component.css']
 })
 export class LiveStreamComponent implements OnInit {
-  youtubeUrl: string = 'https://www.youtube.com/embed/Py2q1zabMu8';
-  youtubeChannelId: string = "UCbTnAG-4nCDE7SnS-FcpblQ";
-  youtubeBaseUrl: string = "https://www.youtube.com/embed/live_stream?channel=";
+  youtubeUrl; //: string = 'https://www.youtube.com/embed/Py2q1zabMu8';
+  //youtubeChannelId: string = "UCbTnAG-4nCDE7SnS-FcpblQ";
+  //youtubeBaseUrl: string = "https://www.youtube.com/embed/live_stream?autoplay=1&channel=";
   
-  constructor(private embedService: EmbedVideoService) {
+  constructor(private embedService: EmbedVideoService,  private sanitizer:DomSanitizer) {
+    //this.Url = this.sanitizer.bypassSecurityTrustResourceUrl(this.pdfSrc)
+    this.youtubeUrl = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/live_stream?autoplay=1&channel=UCbTnAG-4nCDE7SnS-FcpblQ")
 
    }
   ngOnInit(): void {
-    this.youtubeBaseUrl = this.youtubeBaseUrl + this.youtubeChannelId;
-    console.log(this.youtubeBaseUrl);
+    
   }
 
   
